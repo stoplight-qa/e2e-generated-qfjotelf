eb1d24164a2a5f416d03d841236911884f911bc8
qa-lilgk-64-asgo@example.com <qa-lilgk-64-asgo@example.com>
2023-05-11 13:57:11 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16400.stoplight-dev.com/api/v1/projects/cHJqOjE3Njc/spectral.js?branch_slug=main&token=10f26189-f56a-430a-b81e-9d708f740a04";
+export default { extends: ruleset };
